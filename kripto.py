c4f90778f1f81e81ef440b66cb9801b45938fac8
rizkandailham <rizkandailham@gmail.com>
2025-04-10 12:47:58 +0700

    Penambahan koneksi ke oracle

--- kripto.py
+++ kripto.py
@@ -16,3 +16,19 @@ class sandi:
     dec=b64decode(kunci)
     dec=hexlify(dec)
     return dec.decode("ascii")
+  def koneksi_lokal(user,pass):
+      d=r"C:\Users\disdukcapil_adb\github\kripto\instantclient_23_7"
+      oracledb.init_oracle_client(lib_dir=d)
+      connection=oracledb.connect(user=user,password=pass, host="10.16.7.23",port=1521,service_name="local")
+      hubung=connection.cursor()
+      hubung.execute("select nik from demographics")
+      #keluarkan hasil eksekusi seluruhnya fetchall()
+      array=[]
+      for i in hubung.fetchall()
+        array.append(i)
+      #metode 2 menggunakan fetchone()
+      #hasil eksekusi di tarik 1 per 1 disimpan di dalam array
+
+      while True:
+          row=cursor.fetchone()
+          array.append(i)
