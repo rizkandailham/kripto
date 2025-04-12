7efa57f68fa276f1f298b28cab1000025a29f41c
rizkandailham <rizkandailham@gmail.com>
2025-04-13 02:25:29 +0700

    Signed-off-by: rizkandailham <rizkandailham@gmail.com>

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,7 @@
 import pyshorteners
-
+import flask
+import json
+import hashlib
 
 class web:
     def pendek(link):
@@ -10,3 +12,42 @@ class web:
         random.seed(phaseprase)
         s="qwertyuiopasdfghjkl;'[]\1234567890-=!@#$%^&*(()QWERTYUIOPASDFGHJKLZXCVBNM<>?:{{}}123456789+-"
         return "".join(random.sample(s,panjang))
+class web_server:
+    def input_password(self,pass):
+        return hashlib.sha256(pass.encode()).hexdigest()
+
+    def validasi(self,hash,pass):
+        valid=False
+        if hash==hashlib.sha256(pass.encode()).hexdigest():
+                valid=True
+        return valid
+        
+    def reset_password(self,phaseprase):
+        return web.acak(8,phaseprase)
+
+    def buat_password_random(self,phaseprase):
+        return web.acak(random.randint(8,16),phaseprase)
+
+    def registrasi(self):
+
+    def login(self):
+
+    def mining(self):
+
+    def exchange(self):
+
+    def simpan_hasil_mining_data(self):
+
+    def start_mining(self):
+
+    def pilih_algoritma_hashing(self):
+
+    def masukan_phaseprase(self):
+
+    def authenticator_google(self):
+
+    def multifactor_authenticator(self):
+
+    def proses_regression(self):
+
+    def proses_classification(self):
