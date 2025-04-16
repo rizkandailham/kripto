a707c43e51795e37c666dc2f91559facb1f33e19
rizkandailham <rizkandailham@gmail.com>
2025-04-17 02:08:25 +0700

    Signed-off-by: rizkandailham <rizkandailham@gmail.com>

--- Gnum.py
+++ Gnum.py
@@ -1,4 +1,8 @@
 import random
+import kripadd
+import hashlib
+
+hashing=kripadd.kripto_hash()
 
 class guessing:
     def rand_number(self):
@@ -7,16 +11,22 @@ class guessing:
 
     def passive_guess(self,randnum):
         guess=None
+        randnum=hashing.enc_sha256(randnum)
         while randnum!=guess:
             guess=input("Show me your guess lucky number : ")
+            guess=hashing.enc_sha256(str(guess))
         print("Congrats you get the number!")
 
-def dynamic_guess(self):
+    def dynamic_guess(self):
+        random.seed()
         randnum=self.rand_number()
         guess=None
         while randnum!=guess:
             guess=input("Show me your guess lucky number: ")
-            randnum=rand_number()
+            guess=hashing.enc_sha256(str(guess))
+            randnum=hashing.enc_sha256(str(self.rand_number()))
+            #print(randnum)
+            #print(guess)
 if __name__ == "__main__":
     play=guessing()
     play.guessor(play.rand_number())
